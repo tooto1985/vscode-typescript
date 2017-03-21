e737692451bbedd835c94b2af9139434fff26621
tommy <tooto1985@gmail.com>
2017-03-21 12:51:24 +0800

    add

---
+++ dst/test.js
@@ -0,0 +1,5 @@
+function run(name, age) {
+    return name + age.toString();
+}
+run("Mark", 100);
+//# sourceMappingURL=test.js.map
---
+++ src/test.ts
@@ -0,0 +1,4 @@
+function run(name:String, age: Number) {
+    return name + age.toString();
+}
+run("Mark",100);
